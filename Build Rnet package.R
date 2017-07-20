12dfba055de329bc3a3b7f65fa2e452cbf87d22a
WJL-NCSU <wjlove@ncsu.edu>
2017-07-20 02:18:33 -0400

    Removing useless code additions

--- Build Rnet package.R
+++ Build Rnet package.R
@@ -18,13 +18,10 @@ devtools::use_build_ignore(".*.Rproj")
 
 
 document()
-<<<<<<< HEAD
 build()
 sink(file = 'check.txt')
 check()
 sink()
 
-=======
 build(path = '.')
 check()
->>>>>>> dd82ce56a18055308c811db62925214022c2f6da
